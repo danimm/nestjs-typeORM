17e5a3c286a595ad51cbbe7f0cca4335f4b944cd
Dani <danimm1989@gmail.com>
2022-04-24 16:31:47 +0200

    add typeORM

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ export default registerAs('config', () => {
     postgres: {
       database: process.env.POSTGRES_DB,
       port: parseInt(process.env.POSTGRES_PORT, 10),
-      user: process.env.POSTGRES_USER,
+      username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       host: process.env.POSTGRES_HOST,
     },
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,29 +2,46 @@ import { Module, Global } from '@nestjs/common';
 import { Client } from 'pg';
 import config from '../config';
 import { ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Global()
 @Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { username, host, database, password, port } =
+          configService.postgres;
+        return {
+          type: 'postgres',
+          host,
+          port,
+          username,
+          password,
+          database,
+        };
+      },
+    }),
+  ],
   providers: [
     {
       provide: 'PG',
       useFactory: (configService: ConfigType<typeof config>) => {
-        const { user, host, database, password, port } = configService.postgres;
+        const { username, host, database, password, port } =
+          configService.postgres;
         const client = new Client({
-          user,
+          user: username,
           host,
           database,
           password,
           port,
         });
-
         client.connect();
-
         return client;
       },
       inject: [config.KEY],
     },
   ],
-  exports: ['PG'],
+  exports: ['PG', TypeOrmModule],
 })
 export class DatabaseModule {}
