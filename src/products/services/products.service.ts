eb1ec275ae19d5b5a8cfdff667f2c814c284776b
Dani <danimm1989@gmail.com>
2022-05-15 17:53:13 +0200

    filters

--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -4,7 +4,7 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, Between, FindConditions } from 'typeorm';
 
 import { Product } from '../entities/product.entity';
 import {
@@ -26,9 +26,14 @@ export class ProductsService {
 
   findAll(params?: FilterProductsDto) {
     if (params) {
-      const { limit, offset } = params;
+      const { limit, offset, minPrice, maxPrice } = params;
+      const where: FindConditions<Product> = {};
+      if (minPrice && maxPrice) {
+        where.price = Between(minPrice, maxPrice);
+      }
       return this.productRepository.find({
         relations: ['brand'],
+        where,
         take: limit,
         skip: offset,
         order: {
